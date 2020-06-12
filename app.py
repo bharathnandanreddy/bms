1995b832b96f506c732a8a9a9be3bf9aebe08b01
bharath <karla.bharatrhnandan@gmail.com>
2020-06-12 20:47:39 +0530

    updated

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from flask import Flask, render_template , request, redirect, url_for, session
+from flask_fontawesome import FontAwesome
+
 from flask_mysqldb import MySQL
 import MySQLdb.cursors
 import re
+import datetime 
+import time
+
+
 
 
 app = Flask(__name__)
+fa = FontAwesome(app)
 app.secret_key = 'secret_key_007'
 
 # Enter your database connection details below
@@ -43,6 +50,12 @@ def login():
             # Create session data, we can access this data in other routes
             session['loggedin'] = True
             session['username'] = account['user_id']
+
+            ts = time.time()
+            timestamp = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+            print(timestamp)
+            cursor.execute('UPDATE userstore SET timestamp = %s WHERE  user_id = %s;',(timestamp,account['user_id']))
+            mysql.connection.commit()
             # Redirect to home page
             return 'Logged in successfully!'
         else:
